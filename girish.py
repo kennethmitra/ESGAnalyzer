9d03fb24540ff10800afe162b858e473c0569e6e
Andrew Do <andrew.do@utexas.edu>
2019-11-02 16:15:47 -0500

    csv

--- girish.py
+++ girish.py
@@ -4,4 +4,5 @@ from pandas import ExcelFile
 excel_path = "company.xlsx"
 
 data = pd.read_excel(excel_path)
-print(data)
+short_data = data.loc[:, 'TICKER' : 'WORKER']
+short_data.to_csv('data.csv')
