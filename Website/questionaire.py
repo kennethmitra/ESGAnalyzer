ef4a0df960691b9365c44902a563fb587181b914
Andrew Do <andrew.do@utexas.edu>
2019-11-03 11:15:28 -0600

    co

--- Website/questionaire.py
+++ Website/questionaire.py
@@ -11,17 +11,20 @@ def get_altered_df (questionaire):
         size = size + 1
     
     df['WEIGHTED_SCORE'] = df[['COMM', 'CUST', 'ENV', 'JOBS', 'LEAD', 'PROD', 'WORKER']].mean(axis=1)
-    df = df.sort(columns = ['WEIGHTED_SCORE'], ascending = False)
+    df = df.sort_values(['WEIGHTED_SCORE'], ascending = False)
+    
     df = df[['NAME', 'TICKER']].head(10)
     
     for index, row in df.iterrows():
         print(row['NAME'], row['TICKER'], sep = ',', end = '/')
+        
 
 
-inString = sys.argv[1]
+inString ='0,0,9,0,0,0,0'
 inStringArr = inString.split(',')
 inIntArr = list()
 for s in inStringArr:
     inIntArr.append(int(s))
+
 get_altered_df(inIntArr)
 
