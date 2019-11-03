72bcd5bdde1b5c81c4083ef1b3d93a47a82fc18f
Andrew Do <andrew.do@utexas.edu>
2019-11-03 00:36:16 -0500

    done

--- Website/getOther.py
+++ Website/getOther.py
@@ -1,2 +1,13 @@
 import pandas as pd
+import random
 
+df = pd.read_csv("data.csv")
+df2 = pd.read_csv('industrydata.csv')
+companyName = sys.argv[1]
+
+try:
+    matchingCompanies = df.loc[(df['NAME'] == companyName) | (df['TICKER'] == companyName)]
+    firstCompany = matchingCompanies.iloc[0]
+    print(firstCompany['NAME'],firstCompany['TICKER'])
+except:
+    print(companyName,companyName)
--- getESGStats.py
+++ getESGStats.py
@@ -5,8 +5,8 @@ import random
 df = pd.read_csv("data.csv")
 df2 = pd.read_csv('industrydata.csv')
 #print(sys.argv)
-# coPL'mpanyName = sys.argv[1]
-# companyName = 'AA
+companyName = sys.argv[1]
+# companyName = 'AAPL'
 try:
     matchingCompanies = df.loc[(df['NAME'] == companyName) | (df['TICKER'] == companyName)]
     industry = matchingCompanies.iloc[0]['JUST_IND']
