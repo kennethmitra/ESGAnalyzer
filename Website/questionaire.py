89869717f055275e21ff89ada176badceed14087
Kenneth Mitra <kennethmitra@gmail.com>
2019-11-03 11:34:50 -0600

    pish

--- Website/questionaire.py
+++ Website/questionaire.py
@@ -1,30 +1,29 @@
 import pandas as pd
 import sys
 
-def get_altered_df (questionaire):
+
+def get_altered_df(questionaire):
     size = 0
-   
+
     df = pd.read_csv('data.csv')
     # print(df)
     for i in questionaire:
         df.iloc[:, size + 6] *= i
         size = size + 1
-    
-    df['WEIGHTED_SCORE'] = df[['COMM', 'CUST', 'ENV', 'JOBS', 'LEAD', 'PROD', 'WORKER']].mean(axis=1)
-    df = df.sort_values(['WEIGHTED_SCORE'], ascending = False)
-    
+
+    df['WEIGHTED_SCORE'] = df[['ENV', 'WORKER', 'PROD', 'CUST', 'LEAD', 'COMM', 'JOBS']].mean(axis=1)
+    df = df.sort(['WEIGHTED_SCORE'], ascending=False)
+
     df = df[['NAME', 'TICKER']].head(10)
-    
+
     for index, row in df.iterrows():
-        print(row['NAME'], row['TICKER'], sep = ',', end = '/')
-        
+        print(row['NAME'], row['TICKER'], sep=',', end='/')
 
 
-inString ='0,0,9,0,0,0,0'
+inString = sys.argv[1]
 inStringArr = inString.split(',')
 inIntArr = list()
 for s in inStringArr:
     inIntArr.append(int(s))
 
 get_altered_df(inIntArr)
-
