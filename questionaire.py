ef99a642a2209144e1f68f945910195fe1b3e605
Andrew Do <andrew.do@utexas.edu>
2019-11-02 22:58:01 -0500

    questionaire just needs argument passing

--- questionaire.py
+++ questionaire.py
@@ -1,12 +1,23 @@
 import pandas as pd
 
+
+
+
 def get_altered_df (questionaire):
     size = 0
+   
     df = pd.read_csv('data.csv')
+    # print(df)
     for i in questionaire:
-        df.iloc[:,[size + 5]] *= i
+        df.iloc[:, size + 6] *= i
         size = size + 1
     
-    print(df)
+    df['WEIGHTED_SCORE'] = df[['COMM', 'CUST', 'ENV', 'JOBS', 'LEAD', 'PROD', 'WORKER']].mean(axis=1)
+    df = df.sort_values(by = ['WEIGHTED_SCORE'], ascending = False)
+    df = df[['NAME', 'TICKER']].head(10)
+    
+    for index, row in df.iterrows():
+        print(row['NAME'], row['TICKER'], sep = ',', end = ',')
 
-# get_altered_df([1,2,3,4,5,6,7])
+
+
