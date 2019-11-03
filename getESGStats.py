b89833725ad2a650ead1ed5918281ac2d084892a
Andrew Do <andrew.do@utexas.edu>
2019-11-02 23:41:12 -0500

    c

--- getESGStats.py
+++ getESGStats.py
@@ -3,14 +3,16 @@ import sys
 import random
 
 df = pd.read_csv("data.csv")
+df2 = pd.read_csv('industrydata.csv')
 #print(sys.argv)
-companyName = sys.argv[1]
-
+# companyName = sys.argv[1]
+companyName = 'AAPL'
 try:
     matchingCompanies = df.loc[(df['NAME'] == companyName) | (df['TICKER'] == companyName)]
-    
+    industry = matchingCompanies.iloc[0]['JUST_IND']
     firstCompany = matchingCompanies.iloc[0]
-    #print(firstCompany)
+    matchingIndustry = df2.loc[(df['NAME'] == industry)]
+    firstIndustry = matchingIndustry.iloc[0]
     print(firstCompany['NAME'],firstCompany['TICKER'],firstCompany['JUST_IND'],firstCompany['WEIGHTED_SCORE'],firstCompany['RANK'],firstCompany['ENV'],firstCompany['WORKER'],firstCompany['PROD'],firstCompany['CUST'],firstCompany['LEAD'],firstCompany['COMM'], sep=',')
 except:
     print(companyName,companyName,random.randint(50,100),random.randint(42,100),random.randint(42,100),random.randint(42,100),random.randint(42,100),random.randint(42,100),random.randint(42,100),random.randint(42,100),sep=',')
--- girish.py
+++ girish.py
@@ -7,5 +7,5 @@ data = pd.read_excel(excel_path)
 data2 = pd.read_excel(industry_path)
 short_data = data.loc[:, 'TICKER' : 'WORKER']
 short_data.to_csv('data.csv', index = False)
-short_data2 = data.loc[:, 'NAME' : 'WORKER']
+short_data2 = data2.loc[:, 'NAME' : 'WORKER']
 short_data2.to_csv('industrydata.csv', index = False)
