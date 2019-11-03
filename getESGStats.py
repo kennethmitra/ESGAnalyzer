6a4211428fee4914d6f9be566fa4237e1a551c47
Andrew Do <andrew.do@utexas.edu>
2019-11-02 23:43:30 -0500

    c

--- getESGStats.py
+++ getESGStats.py
@@ -13,6 +13,6 @@ try:
     firstCompany = matchingCompanies.iloc[0]
     matchingIndustry = df2.loc[(df['NAME'] == industry)]
     firstIndustry = matchingIndustry.iloc[0]
-    print(firstCompany['NAME'],firstCompany['TICKER'],firstCompany['JUST_IND'],firstCompany['WEIGHTED_SCORE'],firstCompany['RANK'],firstCompany['ENV'],firstCompany['WORKER'],firstCompany['PROD'],firstCompany['CUST'],firstCompany['LEAD'],firstCompany['COMM'], sep=',')
+    print(firstCompany['NAME'],firstCompany['TICKER'],firstCompany['JUST_IND'],firstCompany['WEIGHTED_SCORE'],firstCompany['RANK'],firstCompany['ENV'],firstCompany['WORKER'],firstCompany['PROD'],firstCompany['CUST'],firstCompany['LEAD'],firstCompany['COMM'], firstCompany['JOBS'], sep=',')
 except:
     print(companyName,companyName,random.randint(50,100),random.randint(42,100),random.randint(42,100),random.randint(42,100),random.randint(42,100),random.randint(42,100),random.randint(42,100),random.randint(42,100),sep=',')
