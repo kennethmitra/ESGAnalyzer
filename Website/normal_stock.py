ea9ea8409bb4a07653a241140ac300e5c69ec8b6
Andrew Do <andrew.do@utexas.edu>
2019-11-03 10:51:10 -0600

    s

--- Website/normal_stock.py
+++ Website/normal_stock.py
@@ -7,8 +7,8 @@ def calc_percent(a, b):
     return (a - b)/a * 100
 
 df_now = pd.read_csv('stockdata.csv')
-df_now = df_now.loc[(df_now['symbol'] == companyName), ['symbol','latestPrice', 'change','volume', 'marketCap']]
+df_now = df_now.loc[(df_now['symbol'] == companyName), ['symbol','latestPrice', 'change','volume', 'marketCap', 'changePercent']]
 
 
-print(df_now.iloc[0][0], df_now.iloc[0][1], df_now.iloc[0][2], df_now.iloc[0][3], int(round(float("{:.16f}".format(float(df_now.iloc[0][4]))))), sep=',')
+print(df_now.iloc[0][0], df_now.iloc[0][1], df_now.iloc[0][2], df_now.iloc[0][3], int(round(float("{:.16f}".format(float(df_now.iloc[0][4]))))), df_now.iloc[0][5],sep=',')
 
