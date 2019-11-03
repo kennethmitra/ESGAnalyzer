4bdc8d6bc017c008d12e8d118b6420bac338cc29
Andrew Do <andrew.do@utexas.edu>
2019-11-02 22:22:31 -0500

    commit

--- wiki.py
+++ wiki.py
@@ -2,16 +2,16 @@ import wikipedia as wiki
 import pandas as pd
 
 df = pd.read_csv('data.csv')
-df2 = pd.DataFrame(columns = ['NAMES', 'ABOUT'])
+df2 = pd.DataFrame(columns = ['NAMES', 'TICKER', 'ABOUT'])
 error = pd.DataFrame(columns = ['Index','Company'])
 i = 0
 for index, row in df.iterrows():
     try:
-        df3 = pd.DataFrame({'NAMES' : [row['NAME']], 'ABOUT': [wiki.summary(row['NAME'])]})
+        df3 = pd.DataFrame({'NAMES' : [row['NAME']], 'TICKER': row['TICKER'], 'ABOUT': [wiki.summary(row['NAME'])]})
         df2 = df2.append(df3, ignore_index = True)
     except: 
         error = error.append({'Index' :i,'Company': row['NAME']}, ignore_index = True)
-        df3 = pd.DataFrame({'NAMES' : [row['NAME']], 'ABOUT': ''})
+        df3 = pd.DataFrame({'NAMES' : [row['NAME']], 'TICKER': row['TICKER'], 'ABOUT': ''})
         df2 = df2.append(df3, ignore_index = True)
 
     print(i)
