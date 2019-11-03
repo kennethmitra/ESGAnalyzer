5bab28cc1cab2bb7ec08c526d695de1da4034afb
Andrew Do <andrew.do@utexas.edu>
2019-11-02 20:43:46 -0500

    fix

---
+++ get_stuff.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import sys
+
+data = data.read_csv('data.csv')
+
+def getData():
--- wiki.py
+++ wiki.py
@@ -2,17 +2,17 @@ import wikipedia as wiki
 import pandas as pd
 
 df = pd.read_csv('data.csv')
-df2 = pd.DataFrame(['NAME', 'ABOUT'])
-error = pd.DataFrame(['ERROR'])
+df2 = pd.DataFrame(columns = ['NAMES', 'ABOUT'])
+error = pd.DataFrame(columns = ['Index','Company'])
 i = 0
 for index, row in df.iterrows():
     try:
-        df3 = pd.DataFrame([row['NAME'], wiki.summary(row['NAME'])])
+        df3 = pd.DataFrame({'NAMES' : [row['NAME']], 'ABOUT': [wiki.summary(row['NAME'])]})
     except: 
-        error = error.append([row['NAME']])
+        error = error.append({'Index' :i,'Company': row['NAME']}, ignore_index = True)
     print(i)
     i = i + 1
     df2 = df2.append(df3, ignore_index = True)
-    
-df2.to_csv("about.csv")
-error.to_csv("cant.csv")
+
+df2.to_csv("about.csv", index = False)
+error.to_csv("cant.csv", index = False)
