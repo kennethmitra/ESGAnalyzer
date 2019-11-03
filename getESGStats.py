8b1fb2e94e78ac0c10d4d6ca9fc8296b08bd27e4
Andrew Do <andrew.do@utexas.edu>
2019-11-02 23:59:07 -0500

    done with average

--- getESGStats.py
+++ getESGStats.py
@@ -11,8 +11,10 @@ try:
     matchingCompanies = df.loc[(df['NAME'] == companyName) | (df['TICKER'] == companyName)]
     industry = matchingCompanies.iloc[0]['JUST_IND']
     firstCompany = matchingCompanies.iloc[0]
-    matchingIndustry = df2.loc[(df['NAME'] == industry)]
+    matchingIndustry = df2.loc[(df2['NAME'] == industry)]
     firstIndustry = matchingIndustry.iloc[0]
-    print(firstCompany['NAME'],firstCompany['TICKER'],firstCompany['JUST_IND'],firstCompany['WEIGHTED_SCORE'],firstCompany['RANK'],firstCompany['ENV'],firstCompany['WORKER'],firstCompany['PROD'],firstCompany['CUST'],firstCompany['LEAD'],firstCompany['COMM'], firstCompany['JOBS'], sep=',')
+    print(firstCompany['NAME'],firstCompany['TICKER'],firstCompany['JUST_IND'],firstCompany['WEIGHTED_SCORE'],firstCompany['RANK'],firstCompany['ENV'],firstCompany['WORKER'],firstCompany['PROD'],firstCompany['CUST'],firstCompany['LEAD'],firstCompany['COMM'], firstCompany['JOBS'], firstIndustry['WEIGHTED_SCORE'],firstIndustry['RANK'],firstIndustry['ENV'],firstIndustry['WORKER'],firstIndustry['PROD'],firstIndustry['CUST'],firstIndustry['LEAD'],firstIndustry['COMM'], firstIndustry['JOBS'], sep=',')
 except:
     print(companyName,companyName,random.randint(50,100),random.randint(42,100),random.randint(42,100),random.randint(42,100),random.randint(42,100),random.randint(42,100),random.randint(42,100),random.randint(42,100),sep=',')
+
+
