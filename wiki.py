a62d93d7aa73a02650981fb76a3cb6b2131f65be
Andrew Do <andrew.do@utexas.edu>
2019-11-02 21:33:45 -0500

    correctwikistuff

--- wiki.py
+++ wiki.py
@@ -8,11 +8,15 @@ i = 0
 for index, row in df.iterrows():
     try:
         df3 = pd.DataFrame({'NAMES' : [row['NAME']], 'ABOUT': [wiki.summary(row['NAME'])]})
+        df2 = df2.append(df3, ignore_index = True)
     except: 
         error = error.append({'Index' :i,'Company': row['NAME']}, ignore_index = True)
+        df3 = pd.DataFrame({'NAMES' : [row['NAME']], 'ABOUT': ''})
+        df2 = df2.append(df3, ignore_index = True)
+
     print(i)
     i = i + 1
-    df2 = df2.append(df3, ignore_index = True)
+
 
 df2.to_csv("about.csv", index = False)
 error.to_csv("cant.csv", index = False)
